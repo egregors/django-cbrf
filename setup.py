2ae3662c14001257cb9e94e3f1f83ccd62e9a5d5
Vadim Iskuchekov <egregors@pm.me>
2024-12-09 15:50:29 +0000

    bump 1.0.2

--- setup.py
+++ setup.py
@@ -15,19 +15,18 @@ setup(
     url='https://github.com/Egregors/django-cbrf',
     license='MIT',
     author='Vadim Iskuchekov (@egregors)',
-    author_email='egregors@yandex.ru',
+    author_email='egregors@pm.me',
     description='Django app to integrate Wrapper for The Central Bank of the Russian Federation site API '
                 'with your project',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.8'
     ],
 )
